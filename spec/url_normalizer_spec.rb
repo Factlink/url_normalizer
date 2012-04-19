75f3f63fd2cf297b879585cc46fed19fb240ef84
tomdev <tom@codigy.nl>
2012-04-19 16:01:23 +0200

    Capitalized escaped character
    Signed-off-by: tomdev <tom@codigy.nl>

--- spec/url_normalizer_spec.rb
+++ spec/url_normalizer_spec.rb
@@ -10,14 +10,14 @@ describe UrlNormalizer do
     it { should_normalize_to 'http://www.google.com/foo#bar', 'http://www.google.com/foo' }
 
     ['utm_source', 'utm_medium', 'utm_source', 'utm_content', 'utm_campaign'].each do |strip_param|
-      it "should strip #{strip_param} if it is the only parameter" do 
+      it "should strip #{strip_param} if it is the only parameter" do
         should_normalize_to "http://www.google.com/foo?#{strip_param}=bar",'http://www.google.com/foo'
       end
       it "should strip #{strip_param} if there are other parameters" do
         should_normalize_to "http://www.google.com/foo?#{strip_param}=bar&x=y",'http://www.google.com/foo?x=y'
       end
     end
-    it { should_normalize_to 'http://www.google.com/?x=y|z', 'http://www.google.com/?x=y%7cz' }
+    it { should_normalize_to 'http://www.google.com/?x=y|z', 'http://www.google.com/?x=y%7Cz' }
     it { should_normalize_to 'http://www.google.com/a[b]', 'http://www.google.com/a[b]' }
   end
 
