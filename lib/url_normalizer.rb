a6c29d6f565b980dd1af2165718a4265f72f3166
tomdev <tom@codigy.nl>
2012-05-01 15:27:21 +0200

    [ ] not allowed in URI's, encode them.
    Signed-off-by: tomdev <tom@codigy.nl>

--- lib/url_normalizer.rb
+++ lib/url_normalizer.rb
@@ -1,6 +1,19 @@
 require 'uri'
 require 'cgi'
 
+module URI
+  class << self
+
+    def parse_with_safety(uri)
+      parse_without_safety uri.gsub('[', '%5B').gsub(']', '%5D')
+    end
+
+    alias parse_without_safety parse
+    alias parse parse_with_safety
+
+  end
+end
+
 class UrlNormalizer
   @@normalizer_for = Hash.new(UrlNormalizer)
 
